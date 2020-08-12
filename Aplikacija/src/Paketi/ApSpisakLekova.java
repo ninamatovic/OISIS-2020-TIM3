edb8d0fc135c9808eb2c1e25974df64e523ceaa9
Ana <Ana@DESKTOP-0FBOGEB>
2020-08-12 00:25:41 -0700

    Izgled stranica

---
+++ Aplikacija/src/Paketi/ApSpisakLekova.java
@@ -0,0 +1,197 @@
+package Paketi;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+public class ApSpisakLekova extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTable table_1;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ApSpisakLekova frame = new ApSpisakLekova();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ApSpisakLekova() {
+	
+			
+			
+			getContentPane().setBackground(new Color(102, 153, 153));
+			
+			JButton btnPo = new JButton("Po\u010Detna");
+			btnPo.setBounds(30, 110, 150, 40);
+			btnPo.setBackground(new Color(51, 102, 102));
+			btnPo.setForeground(new Color(255, 255, 255));
+			btnPo.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+			btnPo.setFocusPainted(false);
+			
+			JButton btnPo_1 = new JButton("Lekovi");
+			btnPo_1.setBounds(30, 170, 150, 40);
+			btnPo_1.setForeground(Color.WHITE);
+			btnPo_1.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+			btnPo_1.setFocusPainted(false);
+			btnPo_1.setBackground(new Color(51, 102, 102));
+			
+			JButton btnPo_1_1 = new JButton("Recepti");
+			btnPo_1_1.setBounds(30, 230, 150, 40);
+			btnPo_1_1.setForeground(Color.WHITE);
+			btnPo_1_1.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+			btnPo_1_1.setFocusPainted(false);
+			btnPo_1_1.setBackground(new Color(51, 102, 102));
+			
+			JButton btnPo_1_1_1_1 = new JButton("Korpa");
+			btnPo_1_1_1_1.setBounds(30, 380, 150, 40);
+			btnPo_1_1_1_1.setForeground(Color.WHITE);
+			btnPo_1_1_1_1.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+			btnPo_1_1_1_1.setFocusPainted(false);
+			btnPo_1_1_1_1.setBackground(new Color(51, 102, 102));
+			
+			
+			JLabel lblNewLabel = new JLabel("");
+			lblNewLabel.setBounds(27, 5, 150, 112);
+			lblNewLabel.setIcon(new ImageIcon(Strana1.class.getResource("/Slike/LogoM.png")));
+			getContentPane().setLayout(null);
+			getContentPane().add(btnPo);
+			getContentPane().add(btnPo_1);
+			getContentPane().add(btnPo_1_1);
+			getContentPane().add(btnPo_1_1_1_1);
+			getContentPane().add(lblNewLabel);
+			
+			JTextArea txtrPretraga = new JTextArea();
+			txtrPretraga.setText("Pretraga");
+			txtrPretraga.setFont(new Font("Comic Sans MS", Font.BOLD, 21));
+			txtrPretraga.setBackground(new Color(102, 153, 153));
+			txtrPretraga.setBounds(216, 74, 116, 35);
+			getContentPane().add(txtrPretraga);
+			
+			textField = new JTextField();
+			textField.setBounds(324, 86, 203, 20);
+			getContentPane().add(textField);
+			textField.setColumns(10);
+			
+			JButton btnIzlogujSe = new JButton("Izloguj se");
+			btnIzlogujSe.setForeground(Color.WHITE);
+			btnIzlogujSe.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+			btnIzlogujSe.setFocusPainted(false);
+			btnIzlogujSe.setBackground(new Color(51, 102, 102));
+			btnIzlogujSe.setBounds(584, 21, 150, 40);
+			getContentPane().add(btnIzlogujSe);
+			
+			
+			
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setViewportBorder(null);
+			scrollPane.setBounds(217, 110, 536, 309);
+			getContentPane().add(scrollPane);
+			
+			table = new JTable();
+			table.setFont(new Font("Comic Sans MS", Font.ITALIC, 18));
+			table.setFillsViewportHeight(true);
+			table.setEnabled(false);
+			scrollPane.setViewportView(table);
+			
+			table.setModel(new DefaultTableModel(
+				new Object[][] {
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+				},
+				new String[] {
+					"\u0160ifra leka", "Naziv", "Proizvo\u0111a\u010D", "Recept", "Cena"
+				}
+			) {
+				Class[] columnTypes = new Class[] {
+					String.class, String.class, String.class, Boolean.class, Float.class
+				};
+				public Class getColumnClass(int columnIndex) {
+					return columnTypes[columnIndex];
+				}
+			});
+			table.getColumnModel().getColumn(0).setPreferredWidth(78);
+			table.getColumnModel().getColumn(1).setPreferredWidth(178);
+			table.getColumnModel().getColumn(2).setPreferredWidth(197);
+			table.getColumnModel().getColumn(3).setPreferredWidth(82);
+			table.getColumnModel().getColumn(4).setPreferredWidth(106);
+			table.setForeground(new Color(0, 51, 102));
+			table.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(0, 51, 102), new Color(0, 51, 102), new Color(0, 51, 102), new Color(0, 51, 102)));
+			table.setBackground(new Color(204, 204, 255));
+			
+			JLabel lblNewLabel_1 = new JLabel("Lekovi");
+			lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.BOLD, 35));
+			lblNewLabel_1.setBounds(216, 21, 196, 54);
+			getContentPane().add(lblNewLabel_1);
+			
+			setBackground(Color.WHITE);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			
+			setBounds(100, 100, 800, 500);
+		}
+
+		private ImageIcon createImage(String string) {
+			// TODO Auto-generated method stub
+			return null;
+	}
+}
